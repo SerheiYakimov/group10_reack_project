898b93e1c938b7954e1e7659a0aa1ae3e749b209
Karina Klevetska <karina.klevetska@gmail.com>
2022-02-05 21:44:04 +0200

    start markup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import Logo from '../../images/logo.svg';
+import sprite from '../../svg/sprite.svg';
 import s from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={s.header}>
-      <img className={s.logo} src={Logo} alt="logo"></img>
+      <svg width="90" height="31">
+        <use href={`${sprite}#logo`}></use>
+      </svg>
     </header>
   );
 };
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,6 +4,7 @@ import s from './Hero.module.css';
 const Hero = () => {
   return (
     <section className={s.hero}>
+      {/* <div className={s.bgTopLeft}></div> */}
       <img className={s.heroLogo} src={HeroLogo} alt="hero-logo"></img>
       <p>smart finance</p>
     </section>
