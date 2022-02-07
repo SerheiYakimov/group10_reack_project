a978f7113145f06bf3c6f47efa44d0f99d8ec574
Karina Klevetska <karina.klevetska@gmail.com>
2022-02-07 20:49:57 +0200

    initial commit

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,12 +1,13 @@
 import Hero from '../../components/Hero/Hero';
 import AccessForm from '../../components/AuthMenu/AuthMenu';
+import s from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <>
+    <section className={s.homePage}>
       <Hero />
       <AccessForm />
-    </>
+    </section>
   );
 };
 
