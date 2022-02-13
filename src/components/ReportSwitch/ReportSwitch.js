b838a96eb887fd1682909168a287d35bda6f308a
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2022-02-13 15:54:02 +0200

    add report page switch

---
+++ src/components/ReportSwitch/ReportSwitch.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import s from './ReportSwitch.module.css';
+
+const ReportSwitch = () => {
+  const [ReportSwitchIncomes, setReportSwitchIncomes] = useState(false);
+
+  const onHandleClickLeft = () => {
+    setReportSwitchIncomes(!ReportSwitchIncomes);
+  };
+
+  const onHandleClickRight = () => {
+    setReportSwitchIncomes(!ReportSwitchIncomes);
+  };
+
+  return (
+    <div className={s.reportSwitchWrapper}>
+      <div className={s.reportSwitch}>
+        <div className={s.reportButtonWrapper}>
+          <button
+            type="button"
+            className={s.reportButtonLeft}
+            onClick={onHandleClickLeft}
+          ></button>
+          <span className={s.reportSwitchTitle}>
+            {ReportSwitchIncomes ? 'ДОХОДЫ' : 'РАСХОДЫ'}
+          </span>
+          <button
+            type="button"
+            className={s.reportButtonRight}
+            onClick={onHandleClickRight}
+          ></button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReportSwitch;
