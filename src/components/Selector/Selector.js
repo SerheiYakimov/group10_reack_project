cbaf02961cbfbe156d366c0b5dfd3699efb108e3
SerheiYakimov <ysergey2901@gmail.com>
2022-02-17 17:09:21 +0200

    fix

--- src/components/Selector/Selector.js
+++ src/components/Selector/Selector.js
@@ -3,20 +3,18 @@ import types from '../../json/incomes.json';
 import React, { useState } from 'react';
 import s from './Selector.module.css';
 import Select from 'react-select';
-// import styled from 'styled-components';
-
+// import { useDispatch } from 'react-redux';
 
 const customStyles = {
   control: () => ({
     // backgroundColor: '#f5f6fb',
-    // // display: 'block',
-    // width: 282,
-    // height: 44,
+    // display: 'block',
+    width: 282,
+    height: 44,
   }),
   dropdownIndicator: () => ({
     display: 'none',
   }),
-
 
   indicatorSeparator: () => ({
     display: 'none',
@@ -33,19 +31,31 @@ const expensesOptions = categories.map(e => ({
   label: e.category,
 }));
 
-
 const incomesOptions = types.map(e => ({
   value: e.id,
   label: e.category,
 }));
 
-
 export const OutcomesSelector = () => {
+  const [inputName, setInputName] = useState('');
   const [selected, setSelected] = useState([]);
+
+  // const dispatch = useDispatch();
 
   const optionId = selected.value;
   const optionCategory = selected.label;
   console.log(optionId, optionCategory);
+
+  const handleChange = e => {
+    const { inputName } = e.target;
+    setInputName(inputName);
+  };
+
+  const resetForm = () => {
+    setInputName('');
+    setSelected('');
+  };
+
   return (
     <>
       <input
@@ -53,12 +63,15 @@ export const OutcomesSelector = () => {
         id="js-keyword-input"
         type="text"
         name="name"
+        value={inputName}
+        onChahge={handleChange}
         placeholder="Описание товара"
         required
       />
       <Select
         classNamePrefix="react-select"
         className={s.control}
+        name="selected"
         onChange={setSelected}
         styles={customStyles}
         placeholder="Категория товара"
