241768f55f419f86a4db88f54f0780152c3f3a9e
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2022-02-17 15:10:22 +0200

    add redux for reports

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -3,16 +3,17 @@ import { useSelector, useDispatch } from 'react-redux';
 import s from './Summary.module.css';
 import SummaryTitle from './SummaryTitle';
 import SummaryList from './SummaryList';
-// import reportSelectors from '../../redux/reports/selectors';
-// import reportOperations from '../../redux/reports/operations';
+import reportSelectors from '../../redux/reports/selectors';
+import reportOperations from '../../redux/reports/operations';
+import getSummaryData from '../../redux/reports/operations';
 
 const summaryData = [
-  { id: '1', month: 'Январь', amount: '- 99999999.99' },
-  { id: '2', month: 'Февраль', amount: '999.99' },
-  { id: '3', month: 'Март ', amount: '999999.99' },
-  { id: '4', month: 'Апрель', amount: '99999.99' },
-  { id: '5', month: 'Май', amount: '9999.99' },
-  { id: '6', month: 'Июнь', amount: '- 9.99' },
+  { id: '1', month: 'Январь', amount: ' 15000.00' },
+  { id: '2', month: 'Февраль', amount: '20000.00' },
+  { id: '3', month: 'Март ', amount: '4000.00' },
+  { id: '4', month: 'Апрель', amount: '12000.00' },
+  { id: '5', month: 'Май', amount: '20000.00' },
+  { id: '6', month: 'Июнь', amount: '8000.00' },
 ];
 
 // const summaryData = useSelector(reportSelectors.getSummaryData);
