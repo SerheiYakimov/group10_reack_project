f426159dc121db552928968c586300516d97ae95
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2022-02-17 12:21:07 +0200

    add api-functions for reports

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,13 +1,30 @@
 import Media from 'react-media';
+import { useSelector, useDispatch } from 'react-redux';
 import s from './Summary.module.css';
 import SummaryTitle from './SummaryTitle';
 import SummaryList from './SummaryList';
+// import reportSelectors from '../../redux/reports/selectors';
+// import reportOperations from '../../redux/reports/operations';
+
+const summaryData = [
+  { id: '1', month: 'Январь', amount: '- 99999999.99' },
+  { id: '2', month: 'Февраль', amount: '999.99' },
+  { id: '3', month: 'Март ', amount: '999999.99' },
+  { id: '4', month: 'Апрель', amount: '99999.99' },
+  { id: '5', month: 'Май', amount: '9999.99' },
+  { id: '6', month: 'Июнь', amount: '- 9.99' },
+];
+
+// const summaryData = useSelector(reportSelectors.getSummaryData);
+// console.log(summaryData);
+// const dispatch = useDispatch();
+// dispatch(reportOperations.getSummaryData({ date: '2022-01' }));
 
 const Summary = () => {
   return (
     <section className={s.summarySection}>
       <SummaryTitle />
-      <SummaryList />
+      <SummaryList summaryData={summaryData} />
     </section>
   );
 };
--- src/services/report-api.js
+++ src/services/report-api.js
@@ -8,9 +8,27 @@ async function getReportByCategories(reportData) {
   console.log(`Report by categories:`, data);
   return data;
 }
+async function getReportBySubCategories(reportData) {
+  const { data } = await axiosServer.get(
+    '/transaction/report-subcategory-by-month',
+    reportData,
+  );
+  console.log(`Report by sub-categories:`, data);
+  return data;
+}
+async function getSummaryByMonth(reportData) {
+  const { data } = await axiosServer.get(
+    '/transaction/report-by-six-month',
+    reportData,
+  );
+  console.log(`Report for summary:`, data);
+  return data;
+}
 
 const reportAPI = {
   getReportByCategories,
+  getReportBySubCategories,
+  getSummaryByMonth,
 };
 
 export default reportAPI;
