1d9dffd5dff7674548eab2ffe782de9a30c10918
Karina Klevetska <karina.klevetska@gmail.com>
2022-02-19 16:25:39 +0200

    add logo link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import authSelectors from '../../redux/auth/selectors';
 import sprite from '../../svg/sprite.svg';
 import Navigation from '../Navigation/Navigation';
@@ -8,9 +9,11 @@ const Header = () => {
   const isAuth = useSelector(authSelectors.getIsAuthenticated);
   return (
     <header className={s.header}>
-      <svg width="90" height="31">
-        <use href={`${sprite}#logo`}></use>
-      </svg>
+      <Link to="/balance">
+        <svg width="90" height="31">
+          <use href={`${sprite}#logo`}></use>
+        </svg>
+      </Link>
       {isAuth ? <Navigation /> : null}
     </header>
   );
