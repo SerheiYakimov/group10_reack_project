59a77b392ad92ab8fa38ba8e1da4dd0066543f0b
Anton Kovalchuck <antonkovalchuck@gmail.com>
2022-02-21 01:12:25 +0200

    added work with get queries

--- src/services/transactions-api.js
+++ src/services/transactions-api.js
@@ -6,13 +6,24 @@ const tokenAPI = token;
 
 async function getApiTransactions() {
   const { data } = await axiosServer.get('/transactions');
+  console.log('inside get api trans');
   return data;
 }
 
 async function getApiAllIncomeOutcome(transactionType) {
-  const { data } = await axiosServer.get('/transactions', transactionType);
+  // transactionType must be either "loss" or "income"
+  const { data } = await axiosServer.get(
+    `/transactions/?transactionType=${transactionType}`,
+  );
+  console.log('get all income', data);
   return data;
 }
+
+// async function getApiAllOutcome() {
+//   const { data } = await axiosServer.get('/transactions/?transactionType=loss');
+//   console.log('get all income', data)
+//   return data;
+// }
 
 async function addTransaction(transaction) {
   const { data } = await axiosServer.post('/transactions', transaction);
@@ -26,11 +37,51 @@ async function deleteApiTransaction(id) {
   return data;
 }
 
+async function getApiTotalSumByMonth(date) {
+  // date must be in format YYYY-MM (ex, 2022-02)
+  const { data } = await axiosServer.get(
+    `/transactions/report-sum-by-month?date=${date}`,
+  );
+  return data;
+}
+
+async function getApiSixMonthsReport(type) {
+  // type must be either "loss" or "income"
+  const { data } = await axiosServer.get(
+    `/transactions/report-by-six-month?type=${type}`,
+  );
+  return data;
+}
+
+async function getApiCategoryMonthReport({ date, type }) {
+  // date must be in format YYYY-MM (ex, 2022-02)
+  // type must be either "loss" or "income"
+  console.log('date', date);
+  console.log('type', type);
+  const { data } = await axiosServer.get(
+    `transactions/report-category-by-month?date=${date}&type=${type}`,
+  );
+  return data;
+}
+
+async function getApiSubcategoryMonthReport({ date, alias }) {
+  // date must be in format YYYY-MM (ex, 2022-02)
+  // category in request equals to alias in json/category.json
+  const { data } = await axiosServer.get(
+    `transactions/report-subcategory-by-month?date=${date}&category=${alias}`,
+  );
+  return data;
+}
+
 const transactionsAPI = {
   getApiTransactions,
   getApiAllIncomeOutcome,
   addTransaction,
   deleteApiTransaction,
+  getApiTotalSumByMonth,
+  getApiSixMonthsReport,
+  getApiCategoryMonthReport,
+  getApiSubcategoryMonthReport,
 };
 
 export default transactionsAPI;
