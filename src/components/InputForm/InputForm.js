678ce9a1febeb16c54bfd0cbe23e66f43c5039e6
Anton Kovalchuck <antonkovalchuck@gmail.com>
2022-02-21 22:37:16 +0200

    transaction form added

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -1,11 +1,12 @@
 import AddingCost from '../AddingCost/AddingCost';
 // import AddingIncome from '../AddingIncome/AddingIncome';
-
+import TransactionsForm from '../TransactionForm/TransactionForm';
 const InputForm = () => {
   return (
     <>
       <AddingCost />
       {/* <AddingIncome /> */}
+      <TransactionsForm />
     </>
   );
 };
