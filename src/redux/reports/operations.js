003c514bd21508db74a7bd15d0909dcd52a61c32
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2022-02-22 18:57:36 +0200

    report - redux add operations

--- src/redux/reports/operations.js
+++ src/redux/reports/operations.js
@@ -1,15 +1,12 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import reportAPI from '../../services/report-api';
-import getReportByCategories from '../../services/report-api';
-import getReportBySubCategories from '../../services/report-api';
-import getSummaryByMonth from '../../services/report-api';
 
 export const getIncomesData = createAsyncThunk(
   '/transactions/report-category-by-month',
   async (date, { rejectWithValue }) => {
     try {
       console.log('date', date);
-      const { data } = await getReportByCategories(date);
+      const { data } = await reportAPI.getReportByCategories(date);
       return data;
     } catch (error) {
       return rejectWithValue(error.message);
@@ -21,7 +18,7 @@ export const getOutcomesData = createAsyncThunk(
   '/transactions/report-category-by-month',
   async (date, { rejectWithValue }) => {
     try {
-      const { data } = await getReportByCategories(date);
+      const { data } = await reportAPI.getReportByCategories(date);
       return data;
     } catch (error) {
       return rejectWithValue(error.message);
@@ -33,7 +30,7 @@ export const getCategoryData = createAsyncThunk(
   '/transactions/report-subcategory-by-month',
   async (date, { rejectWithValue }) => {
     try {
-      const { data } = await getReportBySubCategories(date);
+      const { data } = await reportAPI.getReportBySubCategories(date);
       return data;
     } catch (error) {
       return rejectWithValue(error.message);
@@ -54,8 +51,31 @@ export const getSummaryData = createAsyncThunk(
   },
 );
 
-export const getIncomesSum = createAsyncThunk();
-export const getOutcomesSum = createAsyncThunk();
+export const getIncomesSum = createAsyncThunk(
+  '/transactions/total-sum-by-month',
+  async (date, { rejectWithValue }) => {
+    try {
+      const { data } = await reportAPI.getMonthIncomeSum(date);
+      console.log('data from back end', data);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
+
+export const getOutcomesSum = createAsyncThunk(
+  '/transactions/total-sum-by-month',
+  async (date, { rejectWithValue }) => {
+    try {
+      const { data } = await reportAPI.getMonthOutcomeSum(date);
+      console.log('data from back end', data);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  },
+);
 
 const reportOperations = {
   getSummaryData,
