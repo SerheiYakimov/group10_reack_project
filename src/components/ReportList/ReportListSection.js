bb7edf438cb322f81a7cd7363c7718aa9b117543
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2022-02-24 14:08:41 +0200

    add reaction to report-data-switch

--- src/components/ReportList/ReportListSection.js
+++ src/components/ReportList/ReportListSection.js
@@ -4,19 +4,30 @@ import reportOperations from '../../redux/reports/operations';
 import s from './ReportList.module.css';
 import { useEffect } from 'react';
 import ReportList from './ReportList';
+import { getMonth, getYear } from '../../redux/transactions/selectors';
 
 const ReportListSection = ({ ReportSwitchIncomes }) => {
   const dispatch = useDispatch();
 
+  const month = useSelector(getMonth);
+  const year = useSelector(getYear);
+  const reportDataSwitch = year + '-' + month;
+
   useEffect(() => {
     ReportSwitchIncomes
       ? dispatch(
-          reportOperations.getIncomesData({ date: '2022-2', type: 'income' }),
+          reportOperations.getIncomesData({
+            date: reportDataSwitch,
+            type: 'income',
+          }),
         )
       : dispatch(
-          reportOperations.getOutcomesData({ date: '2022-2', type: 'outcome' }),
+          reportOperations.getOutcomesData({
+            date: reportDataSwitch,
+            type: 'outcome',
+          }),
         );
-  }, [dispatch, ReportSwitchIncomes]);
+  }, [dispatch, ReportSwitchIncomes, reportDataSwitch]);
 
   const reportIncomesData = useSelector(reportSelectors.getIncomesData);
   console.log('- incomes', reportIncomesData);
